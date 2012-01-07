f8cd3fe40c01ca9cdacd99049936550309d10cc8
Felix Böhm <me@feedic.com>
2012-01-07 13:45:21 +0100

    Made a number of changes (eg. no more universal event emitters)
    It's getting usable!

--- index.js
+++ index.js
@@ -33,27 +33,31 @@ module.exports.addProtocol = function(name, module){
 *	maxRedirects: int with the maximum number of redirects (defaults to 10)
 *	body: that data that should be passed to the request
 *	encoding: the encoding that all data should use (the body will always be a string)
+*	timeout: a request times out if it passes this limit. Defaults to 10000 (read: 10 seconds)
 */
 
 var Request = function(options, cb){
 	this._options = options;
-	this._headers = null;
+	this._ended = false;
 	this._resp = null;
+	this.response = null;
+	
+	this._cb = cb;
+	this._body = "";
 	
 	this._createRequest(options);
 	if(!this._request) return;
 	
-	this._events = this._request._events = this._request._events || {};
-	
 	var scope = this;
-	if(cb){
-		var body = "";
-		this.on("error", cb).on("end", function(){
-			cb(null, scope._headers, body);
-		}).on("data", function(chunk){
-			body += chunk;
-		});
-	}
+	
+	this.on("error", function(err){
+		scope._cb(err);
+	}).on("end", function(){
+		scope._cb(null, scope._headers, scope._body);
+	}).on("data", function(chunk){
+		if(!scope._written) scope._written = true;
+		scope._body += chunk;
+	});
 	
 	if(options.body) this._request.write(options.body);
 	
@@ -67,7 +71,7 @@ var Request = function(options, cb){
 		if(options.body){
 			this._request.write(options.body);
 		}
-		this.on("pipe", function(src){
+		this.once("pipe", function(src){
 			if(!scope.writable){
 				scope.emit("error", Error("Can't write to socket!"));
 				return;
@@ -78,11 +82,29 @@ var Request = function(options, cb){
 			};
 			src.on("end", cb);
 			src.on("close", cb);
+			
+			scope.on("pipe", function(){
+				throw Error("There is already a pipe");
+			});
 		});
 	}
 };
 
-require("util").inherits(Request, require("stream"));
+var Stream = require("stream").Stream;
+require("util").inherits(Request, Stream);
+
+//save the pipe function for later
+var pipe = Stream.prototype.pipe;
+
+Request.prototype.pipe = function(dest, opts){
+	if(this._written){
+		throw Error("Data was already emitted!");
+	}
+	else if(this._ended){
+		throw Error("Request is closed!");
+	}
+	else pipe.call(this, dest, opts);
+};
 
 Request.prototype._prepareClose = function(){
 	this._close = true;
@@ -128,46 +150,65 @@ Request.prototype._createRequest = function(options){
 	this._request = req.request(options.uri, function(resp){
 		var method = options.uri.method;
 		
-		if( (options.followRedirects || typeof options.followRedirect === "undefined")
-			&& (resp.statusCode % 300 < 99) && !scope.writable){
+		if( (options.followRedirects || typeof options.followRedirect === "undefined") && (resp.statusCode % 300 < 99) && !scope.writable && resp.headers.location){
 				if(!scope._redirected) scope._redirected = 0;
 				
-				scope._request.destroy(); //close the socket - TODO: doesn't work in node < 0.3.8
+				scope._request.abort(); //close the socket
+				clearTimeout(scope._reqTimeout);
+				
+				scope.emit("redirect", resp.headers.location);
 				
 				if(scope._redirected++ < (options.maxRedirects || 10)){
 					if(!re_protocol.test(resp.headers.location)){
 						resp.headers.location = url.resolve(options.uri, resp.headers.location);
 					}
 					
-					this.emit("redirect", resp.headers.location);
-					
-					/* //TODO
 					options.uri = url.parse(resp.headers.location);
 					options.uri.method = method;
-					scope._createRequest(options);
 					
-					//pass the event handlers
-					moveEvents(scope._request, scope);
-					*/
+					scope._createRequest(options);
 				} else {
-					scope._emit("error", Error("Too many redirects"));
+					scope.emit("error", Error("Too many redirects"));
 				}
 				return;
 		}
 		
-		//add some info to the headers object - TODO: find a better way of doing this
-		resp.headers.statusCode = resp.statusCode;
-		resp.headers.location = url.format(options.uri);
+		//add some info to the scope
+		scope.response = {
+			location: url.format(options.uri),
+			statusCode: response.statusCode,
+			headers: resp.headers
+		};
 		
-		scope._headers = resp.headers;
 		scope._resp = resp;
+		
 		if(options.encoding){
 			resp.setEncoding(options.encoding);
 		}
 		
-		//lets use the same event handlers as before
-		moveEvents(resp, scope);
+		resp.on("data", function(chunk){
+			scope.emit("data", chunk);
+		}).on("end", function(){
+			if(!scope._ended) scope.emit("end");
+		}).on("close", function(){
+			scope.emit("close");
+		}).on("error", function(err){
+			scope.emit("error", err);
+		});
+		
+		scope.emit("response", resp);
 	});
+	
+	if(!("timeout" in this._options) || this._options.timeout){
+		this._reqTimeout = setTimeout(function(){
+			if(!scope._ended){
+				scope._request.abort();
+				
+				scope.emit("timeout");
+				scope.emit("error", Error("ETIMEDOUT"));
+			}
+		}, this._options.timeout || 1e4);
+	}
 };
 
 Request.prototype.readable = true;
@@ -178,11 +219,16 @@ Request.prototype.setEncoding = function(encoding){
 	//else, safe it for later
 	else this._options.encoding = encoding;
 };
-Request.prototype.destroy = function(){
-	this._request.destroy();
+Request.prototype.then = function(cb){
+	//for promise-like behavior
+	this._cb = cb;
+};
+Request.prototype.abort = function(){
+	this._request.abort();
 };
 Request.prototype.write = function(chunk){
-	this._request.write(chunk);
+	if(!this.writable) throw Error("Ether request method doesn't support .write or request was sent!");
+	return this._request.write(chunk);
 };
 
 module.exports.Request = Request;
